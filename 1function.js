9115dd993d69181a167a69a4afc539126e5a08fc
comic_prime <86186438+Ushpal@users.noreply.github.com>
2024-02-12 05:27:42 +0000

    function

---
+++ 1function.js
@@ -0,0 +1,24 @@
+function sayMyname(){
+    console.log("k");
+    console.log("a");
+    console.log("l");
+    console.log("i");
+}
+
+
+//sayMyname();       
+
+
+
+function addtwonumbers(num1, num2){
+
+    console.log(num1+num2);
+}
+
+addtwonumbers(3,4);
+
+
+function loginUsermesage(username){
+    return `${username} just logged in.`
+}
+console.log(loginUsermesage("Nepali Babu"))
