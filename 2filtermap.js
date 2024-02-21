cd5ffbebe164eae0fcedef91a730d49417163752
comic_prime <86186438+Ushpal@users.noreply.github.com>
2024-02-21 11:28:55 +0000

    2filtermap

--- 2filtermap.js
+++ 2filtermap.js
@@ -4,7 +4,7 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const newNums = myNumers
                 .map((num) => num * 10 )
-                .map( (num) => num + 1)
+                .map( (num) => num + 1)                   //chaining
                 .filter( (num) => num >= 40)
 
 console.log(newNums);
